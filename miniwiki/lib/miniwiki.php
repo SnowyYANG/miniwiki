e7449e8b134d667ce73fbb8fb8959fdb0f931607
src <src@2ec75876-500a-0410-8855-b1c8b3d45901>
2005-09-04 20:38:30 +0000

    fixed bug when magic_quotes were turned on

--- miniwiki/lib/miniwiki.php
+++ miniwiki/lib/miniwiki.php
@@ -277,17 +277,21 @@
 
     # constructor (do not use directly, use new_request())
     function MW_Request() {
-      $this->page_name = (isset($_REQUEST[MW_REQVAR_PAGE_NAME]) ? $_REQUEST[MW_REQVAR_PAGE_NAME] : MW_DEFAULT_PAGE_NAME);
-      $this->action = (isset($_REQUEST[MW_REQVAR_ACTION]) ? $_REQUEST[MW_REQVAR_ACTION] : MW_DEFAULT_ACTION);
-      $this->revision = (isset($_REQUEST[MW_REQVAR_REVISION]) ? $_REQUEST[MW_REQVAR_REVISION] : MW_REVISION_HEAD);
-      $this->content = (isset($_REQUEST[MW_REQVAR_CONTENT]) ? $_REQUEST[MW_REQVAR_CONTENT] : NULL);
-      $this->message = (isset($_REQUEST[MW_REQVAR_MESSAGE]) ? $_REQUEST[MW_REQVAR_MESSAGE] : NULL);
-      $this->preview = (isset($_REQUEST[MW_REQVAR_PREVIEW]) ? $_REQUEST[MW_REQVAR_PREVIEW] : NULL);
-      $this->old_user = (isset($_REQUEST[MW_REQVAR_OLD_USER]) ? $_REQUEST[MW_REQVAR_OLD_USER] : NULL);
-      $this->user = (isset($_REQUEST[MW_REQVAR_USER]) ? $_REQUEST[MW_REQVAR_USER] : NULL);
-      $this->pass = (isset($_REQUEST[MW_REQVAR_PASS]) ? $_REQUEST[MW_REQVAR_PASS] : NULL);
+      $req_array = $_REQUEST;
+      if (get_magic_quotes_gpc()) {
+        $req_array = array_map("stripslashes", $req_array);
+      }
+      $this->page_name = (isset($req_array[MW_REQVAR_PAGE_NAME]) ? $req_array[MW_REQVAR_PAGE_NAME] : MW_DEFAULT_PAGE_NAME);
+      $this->action = (isset($req_array[MW_REQVAR_ACTION]) ? $req_array[MW_REQVAR_ACTION] : MW_DEFAULT_ACTION);
+      $this->revision = (isset($req_array[MW_REQVAR_REVISION]) ? $req_array[MW_REQVAR_REVISION] : MW_REVISION_HEAD);
+      $this->content = (isset($req_array[MW_REQVAR_CONTENT]) ? $req_array[MW_REQVAR_CONTENT] : NULL);
+      $this->message = (isset($req_array[MW_REQVAR_MESSAGE]) ? $req_array[MW_REQVAR_MESSAGE] : NULL);
+      $this->preview = (isset($req_array[MW_REQVAR_PREVIEW]) ? $req_array[MW_REQVAR_PREVIEW] : NULL);
+      $this->old_user = (isset($req_array[MW_REQVAR_OLD_USER]) ? $req_array[MW_REQVAR_OLD_USER] : NULL);
+      $this->user = (isset($req_array[MW_REQVAR_USER]) ? $req_array[MW_REQVAR_USER] : NULL);
+      $this->pass = (isset($req_array[MW_REQVAR_PASS]) ? $req_array[MW_REQVAR_PASS] : NULL);
       $this->sourcefile = (isset($_FILES[MW_REQVAR_SOURCEFILE]) ? $_FILES[MW_REQVAR_SOURCEFILE] : NULL);
-      $this->destfile = (isset($_REQUEST[MW_REQVAR_DESTFILE]) ? $_REQUEST[MW_REQVAR_DESTFILE] : NULL);
+      $this->destfile = (isset($req_array[MW_REQVAR_DESTFILE]) ? $req_array[MW_REQVAR_DESTFILE] : NULL);
       $this->is_head = ($_SERVER["REQUEST_METHOD"] == "HEAD");
     }
   }

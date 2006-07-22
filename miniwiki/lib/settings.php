808517c9095bb47a1275cc8b7846b6c621f12586
src <src@2ec75876-500a-0410-8855-b1c8b3d45901>
2006-07-22 23:48:34 +0000

    treat all settings as case insensitive

--- miniwiki/lib/settings.php
+++ miniwiki/lib/settings.php
@@ -19,20 +19,33 @@
     var $settings = array();
 
     function set_default($name, $value) {
+      $name = strtolower($name);
       $this->defaults[$name] = $value;
     }
 
     function set($name, $value) {
+      $name = strtolower($name);
       $this->settings[$name] = $value;
     }
 
+    /** @private */
+    function get_global_setting($name) {
+      foreach ($GLOBALS as $key => $value) {
+        if (MW_GLOBAL_SETTINGS_VARIABLE_PREFIX.$name === strtolower($key)) {
+          return $value;
+        }
+      }
+      return null;
+    }
+
     function get($name) {
+      $name = strtolower($name);
       if (isset($this->settings[$name])) {
         debug("MW_Settings: $name set from inside");
         return $this->settings[$name];
-      } elseif (isset($GLOBALS[MW_GLOBAL_SETTINGS_VARIABLE_PREFIX.$name])) {
+      } elseif (($global = $this->get_global_setting($name)) !== null) {
         debug("MW_Settings: $name set from outside");
-        return $GLOBALS[MW_GLOBAL_SETTINGS_VARIABLE_PREFIX.$name];
+        return $global;
       } elseif (isset($this->defaults[$name])) {
         debug("MW_Settings: $name from defaults");
         return $this->defaults[$name];

f3bc42ef2c78c62b44a847120e8cc161275afbd6
src <src@2ec75876-500a-0410-8855-b1c8b3d45901>
2006-11-11 13:04:39 +0000

    makedist script

---
+++ miniwiki/makedist.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# $Id$
+
+version=$1
+echo Making distribution of miniWiki $version
+mkdir packaged
+target=packaged/miniwiki-$version
+mkdir $target
+cp -r index.php userdefs.php README NEWS lib maintenance $target
+find $target -type f -name '*~' -exec rm '{}' ';'
+find $target -type d -name '.svn' -exec rm -rf '{}' ';'
+(cd packaged; tar czvf miniwiki-$version.tar.gz miniwiki-$version)

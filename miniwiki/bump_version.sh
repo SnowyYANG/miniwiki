8b3672111a2fd9f931ae6a2282d1fcd89a5376bb
src <src@2ec75876-500a-0410-8855-b1c8b3d45901>
2006-11-11 13:36:11 +0000

    use bump_version.sh in makedist.sh

--- miniwiki/bump_version.sh
+++ miniwiki/bump_version.sh
@@ -5,7 +5,9 @@
 version=$1
 sed 's/\(define("MW_VERSION", "\)[^"]\+/\1'$version'/' lib/miniwiki.php > lib/miniwiki.php.tmp
 mv lib/miniwiki.php.tmp lib/miniwiki.php
-sed 's/\(PROJECT_NUMBER.*=\).*$/\1 '$version'/' Doxyfile > Doxyfile.tmp
-mv Doxyfile.tmp Doxyfile
+if [ -e Doxyfile ]; then
+  sed 's/\(PROJECT_NUMBER.*=\).*$/\1 '$version'/' Doxyfile > Doxyfile.tmp
+  mv Doxyfile.tmp Doxyfile
+fi
 sed 's/\(miniWiki \).*\( (c)\)/\1'$version'\2/' README > README.tmp
 mv README.tmp README
--- miniwiki/makedist.sh
+++ miniwiki/makedist.sh
@@ -10,4 +10,5 @@ mkdir $target
 cp -r index.php userdefs.php README NEWS lib maintenance $target
 find $target -type f -name '*~' -exec rm '{}' ';'
 find $target -type d -name '.svn' -exec rm -rf '{}' ';'
+(cd $target; ../../bump_version.sh $version)
 (cd packaged; tar czvf miniwiki-$version.tar.gz miniwiki-$version)
